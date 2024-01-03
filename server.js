cf34052b080e11fc09451c07e8a06b31e74e62fc
MaximeArn <maxime-arnould@outlook.fr>
2024-01-03 17:24:05 +0100

    add user model

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const connectDb = require("./db/dbConection");
 
 const server = express();
 const PORT = process.env.PORT || 3000;
+connectDb();
 
 server.listen(PORT, () => {
   console.log(`server started on port : ${PORT}`);
