c69dd3cfc78adf39e1d35ead321f4b4ba39cbdb4
MaximeArn <maxime-arnould@outlook.fr>
2024-01-03 17:53:11 +0100

    register method

---
+++ controllers/auth.js
@@ -0,0 +1,17 @@
+const User = require("../models/user");
+module.exports = {
+  register: async ({ body: { username, password, email } }, res, next) => {
+    try {
+      console.log(username, email, password);
+      const newUser = new User({ username, password, email });
+      await newUser.save();
+      res.status(200).json(newUser);
+    } catch (error) {
+      next(error);
+    }
+  },
+  login: async (req, res, next) => {
+    try {
+    } catch (error) {}
+  },
+};
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const connectDb = require("./db/dbConection");
+const authRouter = require("./routers/auth");
 
 const server = express();
 const PORT = process.env.PORT || 3000;
 connectDb();
+
+server.use(express.json());
+server.use("/auth", authRouter);
 
 server.listen(PORT, () => {
   console.log(`server started on port : ${PORT}`);
